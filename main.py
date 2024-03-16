e5647d38d8ad29f7513706494aedf567ae172ef6
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-03-16 20:39:40 +0300

    Adding a line plot with mock data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
@@ -11,3 +12,16 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky")
                       )
 st.subheader(f"{option} for the next {days} day(s) in {place}")
+
+
+def get_data():
+    dates = ["08-03-2024", "09-03-2024", "10-03-2024"]
+    temperatures = [10, -8, 15]
+    return dates, temperatures
+
+
+d, t = get_data()
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+
+st.plotly_chart(figure)
