1c88d8aa70eb7981ef4cd1b0e2d0cb247d884829
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-03-17 18:38:28 +0300

    Integrate backend with UI

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
-place = st.text_input("Place:")
+city = st.text_input("Place:", placeholder="Tokyo")
 days = st.slider("Forecast Days",
                  min_value=1,
                  max_value=5,
@@ -12,10 +12,26 @@ days = st.slider("Forecast Days",
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky")
                       )
-st.subheader(f"{option} for the next {days} day(s) in {place}")
+st.subheader(f"{option} for the next {days} day(s) in {city}")
 
-data = get_data(place=place, days=days, type=option)
+if city:
+    filtered_data = get_data(city=city, days=days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+    match option:
+        case "Temperature":
+            temperatures = [item["main"]["temp"] for item in filtered_data]
+            dates = [item["dt_txt"] for item in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        case "Sky":
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"
+            }
+            sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=150)
 
-st.plotly_chart(figure)
+
